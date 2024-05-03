e430889f6797ef8c75c914af8172a920ec360a6d
slvg01 <151031347+slvg01@users.noreply.github.com>
2024-05-03 10:57:18 +0200

    final structure change

--- chain.py
+++ chain.py
@@ -1,28 +1,21 @@
-# **utils.py**
+# Create the generative chain for the chatbot
 
 import streamlit as st
-import openai
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferWindowMemory
-#from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
-#from langchain.vectorstores import FAISS
 from langchain_community.vectorstores import FAISS
-#from langchain.embeddings import OpenAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import SystemMessagePromptTemplate
-
-openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
 @st.cache_resource
 def load_chain():
     """
-The `load_chain()` function initializes and configures a conversational retrieval chain for
-answering user questions.
-:return: The `load_chain()` function returns a ConversationalRetrievalChain object.
-"""
+    The function initializes and configures a conversational retrieval chain to answer user questions.
+    It returns a ConversationalRetrievalChain object.
+    """
 
     # Load OpenAI embedding model
     embeddings = OpenAIEmbeddings()
@@ -30,24 +23,24 @@ answering user questions.
     # Load OpenAI chat model
     llm = ChatOpenAI(temperature=0)
 
-    # Load our local FAISS index as a retriever
-    vector_store = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    # Load the local vector database as a retriever
+    vector_store = FAISS.load_local("vector_db", embeddings, allow_dangerous_deserialization=True)
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
 
-    # Create memory 'chat_history'
-    memory = ConversationBufferWindowMemory(k=3, memory_key="chat_history")
+    # Create a memory feature for the chat 
+    memory = ConversationBufferWindowMemory(k=5, memory_key="chat_history")
 
     # Create system prompt
     template = """
-    You are an AI assistant for answering questions about the Blendle Employee Handbook.
-    You are given the following extracted parts of a long document and a question. Provide a conversational answer.
-    If you don't know the answer, just say 'Sorry, I don't know ... 😔. 
-    Don't try to make up an answer.
+    You are an AI assistant for answering questions about the Blendle Employee Handbook given to you.
+    Below You are given contextual information and a question,  provide a conversational answer.
+    Do not build or make up an answer if you do not have supporting information
+    If you don't know the answer, just say 'i am very sorry, i do not have any information on that topic yet, please ask your line manager or HR officer directly. 
     If the question is not about the Blendle Employee Handbook, politely inform them that you are tuned to only answer questions about the Blendle Employee Handbook.
 
     {context}
     Question: {question}
-    Helpful Answer:"""
+    Answer:"""
 
     # Create the Conversational Chain
     chain = ConversationalRetrievalChain.from_llm(llm=llm,
@@ -56,7 +49,7 @@ answering user questions.
                                                   get_chat_history=lambda h: h,
                                                   verbose=True)
 
-    # Add systemp prompt to chain
+    # Add system prompt to chain
     # Can only add it at the end for ConversationalRetrievalChain
     QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
     chain.combine_docs_chain.llm_chain.prompt.messages[0] = SystemMessagePromptTemplate(prompt=QA_CHAIN_PROMPT)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import time
 import streamlit as st
-from utils import load_chain
-import openai
+from chain import load_chain
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Custom image for the app icon and the assistant's avatar
 company_logo = 'https://www.app.nl/wp-content/uploads/2019/01/Blendle.png'
@@ -12,36 +10,31 @@ company_logo = 'https://www.app.nl/wp-content/uploads/2019/01/Blendle.png'
 st.set_page_config(
     page_title="Your Notion Chatbot",
     page_icon=company_logo, 
-    
 )
-#&nbsp;&nbsp
-# put a title on the page
+
+# put a title on the page and line return after
 st.markdown("""
 <h1 style="color: #fc6353;">Welcome to Blendle knowledge base</h1>
 <h1 style="color: #fc6353;">Happy to see you again colleague ❤</h1>
-
 """, unsafe_allow_html=True)
 
-for _ in range(4):
+for _ in range(3):
    st.markdown("""
     """)
    
    
 # Initialize LLM chain
-# llm = OpenAI(client=OpenAI, streaming=True, callbacks=[StreamlitCallbackHandler(message_placeholder)])
 chain = load_chain()
 
-# Initialize chat history
 
+# Initialize chat history
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant",
                                   "content": "Hi, I am Blendle's smart AI. Ask me anything about the company policies"}]
 
 
-# Display chat messages from history on app rerun
-# Custom avatar for the assistant, default avatar for user
-
+# Display chat messages from history on app rerun and put a custom avatar for the assistant, default avatar for user
 for message in st.session_state.messages:
     if message["role"] == 'assistant':
         with st.chat_message(message["role"], avatar=company_logo):
@@ -50,7 +43,9 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-# Chat logic
+
+# Chat logical sequence
+#add a base invite message in the chat box 
 if query := st.chat_input("Ask me anything"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": query})
@@ -60,17 +55,15 @@ if query := st.chat_input("Ask me anything"):
 
     with st.chat_message("assistant",avatar=company_logo):
         message_placeholder = st.empty()
-        # Send user's question to our chain
+        # Send user's question to the chain
         result = chain.invoke({"question": query})
-        # result = chain.run(input=query)
-        # result = st.session_state['chain']({"question": query})
         response = result['answer']
         full_response = ""
 
         # Simulate stream of response with milliseconds delay
         for chunk in response.split():
             full_response += chunk + " "
-            time.sleep(0.05)
+            time.sleep(0.12)
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
